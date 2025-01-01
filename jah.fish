25af8c9df7321bdf16e6f2e733d4c68c6f33ac19
rustaroots <rootsrustafari@gmail.com>
2025-01-01 02:53:04 +0100

    replace subcommand by arguments
    add jah.fish for completion on fish shell
    
    $ jah -c|--commit : run jah in commit mode
    
    $ jah -p|--push   : run jah in send to remote mode
    
    $ jah -l|--log	  : run jah in log display mode

---
+++ jah.fish
@@ -0,0 +1,3 @@
+complete -c jah -l commit -s c -d 'Run test and commit helper if all test passe'
+complete -c jah -l push -s p -d 'Send modifications to remotes'
+complete -c jah -l log  -s l -d 'Show only last 5 logs'
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
 #![allow(clippy::multiple_crate_versions)]
 
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command};
 use jah::Jah;
 mod jah;
 
 fn app() -> ArgMatches {
     Command::new("jah")
-        .subcommand(Command::new("commit"))
-        .subcommand(Command::new("push"))
-        .subcommand(Command::new("log"))
+        .arg(
+            Arg::new("commit")
+                .short('c')
+                .long("commit")
+                .num_args(0)
+                .help("Run test and commit on success"),
+        )
+        .arg(
+            Arg::new("push")
+                .short('p')
+                .long("push")
+                .num_args(0)
+                .help("Send modifications to remotes"),
+        )
+        .arg(
+            Arg::new("log")
+                .short('l')
+                .long("log")
+                .num_args(0)
+                .help("Show last 5 last log"),
+        )
         .get_matches()
 }
 fn main() {
     let app = app();
-    if app.subcommand_matches("commit").is_some() {
+    if app.get_flag("commit") {
         Jah::commit();
-    } else if app.subcommand_matches("push").is_some() {
+    } else if app.get_flag("push") {
         Jah::send();
-    } else if app.subcommand_matches("log").is_some() {
+    } else if app.get_flag("log") {
         Jah::log();
     }
 }
